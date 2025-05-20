6669db1a3edc2c112c59ce0dcc43c1f0e1cec26c
Bazket <quacmichael@gmail.com>
2025-05-20 23:55:47 +0800

    Render scores from file

--- Scene/ScoreScene.cpp
+++ Scene/ScoreScene.cpp
@@ -1,14 +1,58 @@
 #include "ScoreScene.hpp"
+#include <fstream>
 #include <UI/Component/ImageButton.hpp>
+#include <UI/Component/Label.hpp>
 #include <Engine/GameEngine.hpp>
+#include <Scene/PlayScene.hpp>
 
-void ScoreScene::Initialize(){
-    IScene::Initialize();
+const int ScoreScene::entriesPerPage = 7;
+const int ScoreScene::scoreboardHeightOffset = 100;
+
+std::vector<ScoreEntry> ScoreScene::readScoreFile(std::string filename)
+{
+    std::ifstream file(filename);
+    std::vector<ScoreEntry> ret;
+    while(!file.eof()){
+        ScoreEntry entry;
+        file>>entry.name>>entry.score>>entry.timestamp;
+        ret.push_back(entry);
+    }
+    file.close();
+    return ret;
+}
+void ScoreScene::writeScoreFile(const std::vector<ScoreEntry> &entries)
+{
+}
+void ScoreScene::drawScores(int page)
+{
+    for(int i = entriesPerPage * page; i < entriesPerPage * (page + 1) && i < entries.size(); i++){
+        labels[i][0]->Text = entries[i].name;
+        labels[i][1]->Text = std::to_string(entries[i].score);
+        labels[i][2]->Text = std::ctime(&entries[i].timestamp);
+    }
+}
+void ScoreScene::Initialize()
+{
+    score = dynamic_cast<PlayScene*>(Engine::GameEngine::GetInstance().GetScene("play"))->score;
+    entries = readScoreFile("Resource/scoreboard.txt");
+    int w = Engine::GameEngine::GetInstance().GetScreenSize().x;
+    int h = Engine::GameEngine::GetInstance().GetScreenSize().y;
+
     Engine::ImageButton *btn;
     btn = new Engine::ImageButton("svgs/regular/circle-left.svg", "svgs/solid/circle-arrow-left.svg", 10, 10, 50, 50);
     btn->SetOnClickCallback(std::bind(&ScoreScene::BackOnClick, this, 1));
     AddNewControlObject(btn);
+    AddNewObject(new Engine::Label("SCOREBOARD", "pirulen.ttf", 48, w / 2, 50, 255, 255, 255, 255, 0.5, 0.5));
 
+    labels.clear();
+    labels.resize(entriesPerPage);
+    for(int i = 0; i < entriesPerPage; i++){
+        labels[i][0] = new Engine::Label("", "pirulen.ttf", 24, w / 2 - 500, scoreboardHeightOffset + i * 35, 255, 255, 255, 255, 0, 0.5);
+        labels[i][1] = new Engine::Label("", "pirulen.ttf", 24, w / 2 - 200, scoreboardHeightOffset + i * 35, 255, 255, 255, 255, 0, 0.5);
+        labels[i][2] = new Engine::Label("", "pirulen.ttf", 24, w / 2, scoreboardHeightOffset + i * 35, 255, 255, 255, 255, 0, 0.5);
+        for(Engine::Label* l : labels[i]) AddNewObject(l);
+    }
+    drawScores(0);
 }
 void ScoreScene::Terminate(){
     IScene::Terminate();    
--- Scene/ScoreScene.hpp
+++ Scene/ScoreScene.hpp
@@ -1,11 +1,31 @@
 #pragma once
 #include "Engine/IScene.hpp"
 #include <allegro5/allegro_audio.h>
+#include <ctime>
+#include <vector>
+#include <string>
+#include <array>
+
+#include <UI/Component/Label.hpp>
+
+struct ScoreEntry{
+    std::string name;
+    int score;
+    std::time_t timestamp;
+};
 
 class ScoreScene final : public Engine::IScene {
 private:
+    static const int entriesPerPage;
+    static const int scoreboardHeightOffset;
     float ticks;
     ALLEGRO_SAMPLE_ID bgmId;
+    std::vector<ScoreEntry> entries;
+    std::vector<std::array<Engine::Label*, 3>> labels;
+    int score;
+    std::vector<ScoreEntry> readScoreFile(std::string filename);
+    void writeScoreFile(const std::vector<ScoreEntry>& entries);
+    void drawScores(int page);
 
 public:
     explicit ScoreScene() = default;
